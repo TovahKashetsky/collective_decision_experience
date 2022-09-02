ad4e14498f8945ee2e81c5d357e144a5425e438c
Janice Yan <janyan33@gmail.com>
2022-09-02 15:06:38 -0400

    Diagram updates

--- scripts/ant_networks.R
+++ scripts/ant_networks.R
@@ -60,13 +60,14 @@ func_igraph <- function(trial_list, emi){
 func_plot_igraph <- function(igraph){
     V(igraph)$size <- (V(igraph)$strength+2)*5
     V(igraph)$label.color <- ifelse(V(igraph)$colour == "blue", "white", "black")
-    V(igraph)$label.cex <- 0.6
+    V(igraph)$label.cex <- 0.65
     V(igraph)$color <- "white"
     V(igraph)$color <- ifelse(V(igraph)$colour == "blue", "#118ab2", ifelse(V(igraph)$colour == "yellow", "#e9c46a",
                                                                      ifelse(V(igraph)$colour == "green", "#a6c48a", "#d9dcd6")))
     E(igraph)$width <- E(igraph)$weight*3
     E(igraph)$lty <- ifelse(E(igraph)$TYPE == "tandem_run", 2, 1)
     E(igraph)$color <- ifelse(E(igraph)$nest == "dark", "#118ab2", "#ffc300")
+    E(igraph)$curved <- TRUE
     
   return(igraph)
 }
@@ -133,14 +134,19 @@ c55_list <- split(c55, c55$Trial) # Creates a list for the three trials
 nodes <- read.csv("data/node_lists.csv") %>% 
          filter(colony == 55) 
 
-igraphs_55 <- lapply(c55_list, func_igraph) 
+igraphs_55_1 <- func_igraph(c55_list[[1]], emi = 1)
+igraphs_55_2 <- func_igraph(c55_list[[2]], emi = 2)
+igraphs_55_3 <- func_igraph(c55_list[[3]], emi = 3)
+
+igraphs_55 <- list(igraphs_55_1, igraphs_55_2, igraphs_55_3)
+
 igraphs_55 <- lapply(igraphs_55, func_plot_igraph) # Creates igraphs for all three trials
 
 # Remove dead ants
-igraphs_55[[3]] <- delete_vertices(igraphs_55[[3]], "w")
+#igraphs_55[[3]] <- delete_vertices(igraphs_55[[3]], "w")
 
 # Trial #1
-id <- tkplot(igraphs_55[[1]])
+id <- tkplot(igraphs_55[[1]], vertex.label.family = "Helvetica")
 coords_55 <- tk_coords(id) # Saves coordinates of trial #1
 tk_set_coords(id, coords_55) # Set stored coordinates to trial #1
 tkplot.fit.to.screen(id)
@@ -151,8 +157,8 @@ hist(V(igraphs_55[[1]])$strength, col = "lightsteelblue1",
 (cv_55_1 <- (sd(V(igraphs_55[[1]])$strength))/(mean(V(igraphs_55[[1]])$strength))) # Coefficient of variation
 
 # Trial #2 
-id <- tkplot(igraphs_55[[2]])
-tkplot(igraphs_55[[2]])
+id <- tkplot(igraphs_55[[2]], vertex.label.family = "Helvetica")
+#tkplot(igraphs_55[[2]])
 
 tk_set_coords(id, coords_55) # Set stored coordinates to trial #2
 tkplot.fit.to.screen(id)
@@ -163,7 +169,7 @@ hist(V(igraphs_55[[2]])$strength, col = "lightsteelblue1",
 (cv_55_2 <- (sd(V(igraphs_55[[2]])$strength))/(mean(V(igraphs_55[[2]])$strength))) # Coefficient of variation
 
 # Trial #3
-id <- tkplot(igraphs_55[[3]])
+id <- tkplot(igraphs_55[[3]], vertex.label.family = "Helvetica")
 
 tk_set_coords(id, coords_55) # Set stored coordinates to trial #2
 tkplot.fit.to.screen(id)
@@ -182,15 +188,20 @@ c41_list <- split(c41, c41$Trial) # Creates a list for the three trials
 nodes <- read.csv("data/node_lists.csv") %>% 
   filter(colony == 41)
 
-igraphs_41 <- lapply(c41_list, func_igraph) 
+igraphs_41_1 <- func_igraph(c41_list[[1]], emi = 1)
+igraphs_41_2 <- func_igraph(c41_list[[2]], emi = 2)
+igraphs_41_3 <- func_igraph(c41_list[[3]], emi = 3)
+
+igraphs_41 <- list(igraphs_41_1, igraphs_41_2, igraphs_41_3)
+
 igraphs_41 <- lapply(igraphs_41, func_plot_igraph) # Creates igraphs for all three trials
 
 # Remove dead ants
-igraphs_41[[2]] <- delete_vertices(igraphs_41[[2]], c("bgy", "bgo"))
-igraphs_41[[3]] <- delete_vertices(igraphs_41[[3]], c("bgy", "bgo"))
+#igraphs_41[[2]] <- delete_vertices(igraphs_41[[2]], c("bgy", "bgo"))
+#igraphs_41[[3]] <- delete_vertices(igraphs_41[[3]], c("bgy", "bgo"))
 
 # Trial #1
-id <- tkplot(igraphs_41[[1]])
+id <- tkplot(igraphs_41[[1]], vertex.label.family = "Helvetica")
 coords_41 <- tk_coords(id) # Saves coordinates of trial #1
 tk_set_coords(id, coords_41) # Set stored coordinates to trial #1
 tkplot.fit.to.screen(id)
@@ -201,7 +212,7 @@ hist(V(igraphs_41[[1]])$strength, col = "lightsteelblue1",
 (cv_41_1 <- (sd(V(igraphs_41[[1]])$strength))/(mean(V(igraphs_41[[1]])$strength))) # Coefficient of variation
 
 # Trial #2 
-id <- tkplot(igraphs_41[[2]])
+id <- tkplot(igraphs_41[[2]], vertex.label.family = "Helvetica")
 
 tk_set_coords(id, coords_41) # Set stored coordinates to trial #2
 tkplot.fit.to.screen(id)
@@ -212,7 +223,7 @@ hist(V(igraphs_41[[2]])$strength, col = "lightsteelblue1",
 (cv_41_2 <- (sd(V(igraphs_41[[2]])$strength))/(mean(V(igraphs_41[[2]])$strength))) # Coefficient of variation
 
 # Trial #3 
-id <- tkplot(igraphs_41[[3]])
+id <- tkplot(igraphs_41[[3]], vertex.label.family = "Helvetica")
 
 tk_set_coords(id, coords_41) # Set stored coordinates to trial #2
 tkplot.fit.to.screen(id)
@@ -231,15 +242,20 @@ c53_list <- split(c53, c53$Trial) # Creates a list for the three trials
 nodes <- read.csv("data/node_lists.csv") %>% 
          filter(colony == 53)
 
-igraphs_53 <- lapply(c53_list, func_igraph) 
+igraphs_53_1 <- func_igraph(c53_list[[1]], emi = 1)
+igraphs_53_2 <- func_igraph(c53_list[[2]], emi = 2)
+igraphs_53_3 <- func_igraph(c53_list[[3]], emi = 3)
+
+igraphs_53 <- list(igraphs_53_1, igraphs_53_2, igraphs_53_3)
+
 igraphs_53 <- lapply(igraphs_53, func_plot_igraph) # Creates igraphs for all three trials
 
 # Remove dead ants
-igraphs_53[[2]] <- delete_vertices(igraphs_53[[2]], "bgw")
-igraphs_53[[3]] <- delete_vertices(igraphs_53[[3]], "bgw")
+# igraphs_53[[2]] <- delete_vertices(igraphs_53[[2]], "bgw")
+# igraphs_53[[3]] <- delete_vertices(igraphs_53[[3]], "bgw")
 
 # Trial #1
-id <- tkplot(igraphs_53[[1]])
+id <- tkplot(igraphs_53[[1]], vertex.label.family = "Helvetica")
 coords_53 <- tk_coords(id) # Saves coordinates of trial #1
 tk_set_coords(id, coords_53) # Set stored coordinates to trial #1
 tkplot.fit.to.screen(id)
@@ -250,7 +266,7 @@ hist(V(igraphs_53[[1]])$strength, col = "lightsteelblue1",
 (cv_53_1 <- (sd(V(igraphs_53[[1]])$strength))/(mean(V(igraphs_53[[1]])$strength))) # Coefficient of variation
 
 # Trial #2 
-id <- tkplot(igraphs_53[[2]])
+id <- tkplot(igraphs_53[[2]], vertex.label.family = "Helvetica")
 
 tk_set_coords(id, coords_53) # Set stored coordinates to trial #2
 tkplot.fit.to.screen(id)
@@ -261,7 +277,7 @@ hist(V(igraphs_53[[2]])$strength, col = "lightsteelblue1",
 (cv_53_2 <- (sd(V(igraphs_53[[2]])$strength))/(mean(V(igraphs_53[[2]])$strength))) # Coefficient of variation
 
 # Trial #3 
-id <- tkplot(igraphs_53[[3]])
+id <- tkplot(igraphs_53[[3]], vertex.label.family = "Helvetica")
 
 tk_set_coords(id, coords_53) # Set stored coordinates to trial #2
 tkplot.fit.to.screen(id)
@@ -279,13 +295,17 @@ c60 <- all_data %>%
 c60_list <- split(c60, c60$Trial) # Creates a list for the three trials
 
 nodes <- read.csv("data/node_lists.csv") %>% 
-  filter(colony == 60)
+         filter(colony == 60)
 
-igraphs_60 <- lapply(c60_list, func_igraph) 
+igraphs_60_1 <- func_igraph(c60_list[[1]], emi = 1)
+igraphs_60_2 <- func_igraph(c60_list[[2]], emi = 2)
+igraphs_60_3 <- func_igraph(c60_list[[3]], emi = 3)
+
+igraphs_60 <- list(igraphs_60_1, igraphs_60_2, igraphs_60_3)
 igraphs_60 <- lapply(igraphs_60, func_plot_igraph) # Creates igraphs for all three trials
 
 # Trial #1
-id <- tkplot(igraphs_60[[1]])
+id <- tkplot(igraphs_60[[1]], vertex.label.family = "Helvetica")
 coords_60 <- tk_coords(id) # Saves coordinates of trial #1
 tk_set_coords(id, coords_60) # Set stored coordinates to trial #1
 tkplot.fit.to.screen(id)
@@ -296,7 +316,7 @@ hist(V(igraphs_60[[1]])$strength, col = "lightsteelblue1",
 (cv_60_1 <- (sd(V(igraphs_60[[1]])$strength))/(mean(V(igraphs_60[[1]])$strength))) # Coefficient of variation
 
 # Trial #2 
-id <- tkplot(igraphs_60[[2]])
+id <- tkplot(igraphs_60[[2]], vertex.label.family = "Helvetica")
 
 tk_set_coords(id, coords_60) # Set stored coordinates to trial #2
 tkplot.fit.to.screen(id)
@@ -307,7 +327,7 @@ hist(V(igraphs_60[[2]])$strength, col = "lightsteelblue1",
 (cv_60_2 <- (sd(V(igraphs_60[[2]])$strength))/(mean(V(igraphs_60[[2]])$strength))) # Coefficient of variation
 
 # Trial #3 
-id <- tkplot(igraphs_60[[3]])
+id <- tkplot(igraphs_60[[3]], vertex.label.family = "Helvetica")
 
 tk_set_coords(id, coords_60) # Set stored coordinates to trial #2
 tkplot.fit.to.screen(id)
@@ -326,15 +346,20 @@ c59_list <- split(c59, c59$Trial) # Creates a list for the three trials
 nodes <- read.csv("data/node_lists.csv") %>% 
   filter(colony == 59)
 
-igraphs_59 <- lapply(c59_list, func_igraph) 
+igraphs_59_1 <- func_igraph(c59_list[[1]], emi = 1)
+igraphs_59_2 <- func_igraph(c59_list[[2]], emi = 2)
+igraphs_59_3 <- func_igraph(c59_list[[3]], emi = 3)
+
+igraphs_59 <- list(igraphs_59_1, igraphs_59_2, igraphs_59_3)
+
 igraphs_59 <- lapply(igraphs_59, func_plot_igraph) # Creates igraphs for all three trials
 
 # Remove dead ants
-igraphs_59[[2]] <- delete_vertices(igraphs_59[[2]], c("gow", "gwg"))
-igraphs_59[[3]] <- delete_vertices(igraphs_59[[3]], c("gow", "gwg"))
+# igraphs_59[[2]] <- delete_vertices(igraphs_59[[2]], c("gow", "gwg"))
+# igraphs_59[[3]] <- delete_vertices(igraphs_59[[3]], c("gow", "gwg"))
 
 # Trial #1
-id <- tkplot(igraphs_59[[1]])
+id <- tkplot(igraphs_59[[1]], vertex.label.family = "Helvetica")
 coords_59 <- tk_coords(id) # Saves coordinates of trial #1
 tk_set_coords(id, coords_59) # Set stored coordinates to trial #1
 tkplot.fit.to.screen(id)
@@ -345,7 +370,7 @@ hist(V(igraphs_59[[1]])$strength, col = "lightsteelblue1",
 (cv_59_1 <- (sd(V(igraphs_59[[1]])$strength))/(mean(V(igraphs_59[[1]])$strength)))
 
 # Trial #2 
-id <- tkplot(igraphs_59[[2]])
+id <- tkplot(igraphs_59[[2]], vertex.label.family = "Helvetica")
 
 tk_set_coords(id, coords_59) # Set stored coordinates to trial #2
 tkplot.fit.to.screen(id)
@@ -356,7 +381,7 @@ hist(V(igraphs_59[[2]])$strength, col = "lightsteelblue1",
 (cv_59_2 <- (sd(V(igraphs_59[[2]])$strength))/(mean(V(igraphs_59[[2]])$strength)))
 
 # Trial #3 
-id <- tkplot(igraphs_59[[3]])
+id <- tkplot(igraphs_59[[3]], vertex.label.family = "Helvetica")
 
 tk_set_coords(id, coords_59) # Set stored coordinates to trial #2
 tkplot.fit.to.screen(id)
@@ -375,16 +400,20 @@ c45_list <- split(c45, c45$Trial) # Creates a list for the three trials
 nodes <- read.csv("data/node_lists.csv") %>% 
   filter(colony == 45)
 
-igraphs_45 <- lapply(c45_list, func_igraph) 
+igraphs_45_1 <- func_igraph(c45_list[[1]], emi = 1)
+igraphs_45_2 <- func_igraph(c45_list[[2]], emi = 2)
+igraphs_45_3 <- func_igraph(c45_list[[3]], emi = 3)
+
+igraphs_45 <- list(igraphs_45_1, igraphs_45_2, igraphs_45_3)
 igraphs_45 <- lapply(igraphs_45, func_plot_igraph) # Creates igraphs for all three trials
 
 # Remove dead ants
-igraphs_45[[1]] <- delete_vertices(igraphs_45[[1]], "wbw")
-igraphs_45[[2]] <- delete_vertices(igraphs_45[[2]], c("bgo", "wbw", "wwy"))
-igraphs_45[[3]] <- delete_vertices(igraphs_45[[3]], c("bgo", "wbw", "wwy"))
+# igraphs_45[[1]] <- delete_vertices(igraphs_45[[1]], "wbw")
+# igraphs_45[[2]] <- delete_vertices(igraphs_45[[2]], c("bgo", "wbw", "wwy"))
+# igraphs_45[[3]] <- delete_vertices(igraphs_45[[3]], c("bgo", "wbw", "wwy"))
 
 # Trial #1
-id <- tkplot(igraphs_45[[1]])
+id <- tkplot(igraphs_45[[1]], vertex.label.family = "Helvetica")
 coords_45 <- tk_coords(id) # Saves coordinates of trial #1
 tk_set_coords(id, coords_45) # Set stored coordinates to trial #1
 tkplot.fit.to.screen(id)
@@ -395,7 +424,7 @@ hist(V(igraphs_45[[1]])$strength, col = "lightsteelblue1",
 (cv_45_1 <- (sd(V(igraphs_45[[1]])$strength))/(mean(V(igraphs_45[[1]])$strength)))
 
 # Trial #2 
-id <- tkplot(igraphs_45[[2]])
+id <- tkplot(igraphs_45[[2]], vertex.label.family = "Helvetica")
 
 tk_set_coords(id, coords_45) # Set stored coordinates to trial #2
 tkplot.fit.to.screen(id)
@@ -406,7 +435,7 @@ hist(V(igraphs_45[[2]])$strength, col = "lightsteelblue1",
 (cv_45_2 <- (sd(V(igraphs_45[[2]])$strength))/(mean(V(igraphs_45[[2]])$strength)))
 
 # Trial #3 
-id <- tkplot(igraphs_45[[3]])
+id <- tkplot(igraphs_45[[3]], vertex.label.family = "Helvetica")
 
 tk_set_coords(id, coords_45) # Set stored coordinates to trial #2
 tkplot.fit.to.screen(id)
@@ -425,15 +454,19 @@ c47_list <- split(c47, c47$Trial) # Creates a list for the three trials
 nodes <- read.csv("data/node_lists.csv") %>% 
   filter(colony == 47)
 
-igraphs_47 <- lapply(c47_list, func_igraph) 
+igraphs_47_1 <- func_igraph(c47_list[[1]], emi = 1)
+igraphs_47_2 <- func_igraph(c47_list[[2]], emi = 2)
+
+igraphs_47 <- list(igraphs_47_1, igraphs_47_2)
 igraphs_47 <- lapply(igraphs_47, func_plot_igraph) # Creates igraphs for all three trials
 
-igraphs_47[[1]] <- delete_vertices(igraphs_47[[1]], "wgo")
-igraphs_47[[2]] <- delete_vertices(igraphs_47[[2]], "wgo")
+# Delete dead ants
+# igraphs_47[[1]] <- delete_vertices(igraphs_47[[1]], "wgo")
+# igraphs_47[[2]] <- delete_vertices(igraphs_47[[2]], "wgo")
 
 # Data from trial 1 is missing
 # Trial #2
-id <- tkplot(igraphs_47[[1]])
+id <- tkplot(igraphs_47[[1]], vertex.label.family = "Helvetica")
 coords_47 <- tk_coords(id) # Saves coordinates of trial #1
 tk_set_coords(id, coords_47) # Set stored coordinates to trial #1
 tkplot.fit.to.screen(id)
@@ -444,7 +477,7 @@ hist(V(igraphs_47[[1]])$strength, col = "lightsteelblue1",
 (cv_45_2 <- (sd(V(igraphs_47[[1]])$strength))/(mean(V(igraphs_47[[1]])$strength)))
 
 # Trial #3
-id <- tkplot(igraphs_47[[2]])
+id <- tkplot(igraphs_47[[2]], vertex.label.family = "Helvetica")
 
 tk_set_coords(id, coords_47) # Set stored coordinates to trial #2
 tkplot.fit.to.screen(id)
